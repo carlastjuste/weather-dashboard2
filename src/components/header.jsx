903b38a66d274fd8c04462a09631fa8808303da0
carlastjuste <carlastjuste@users.noreply.github.com>
2021-03-11 02:10:19 -0500

    Add Header

---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header  extends Component {
+
+    render() { 
+        return ( <div>
+              <header>
+                <h1>Weather Dashboard</h1>
+              </header>
+        </div> );
+    }
+}
+ 
+export default Header ;
