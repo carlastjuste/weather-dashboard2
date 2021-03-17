4e007a4d4bdf5b7495c72b8bcbd40d5243036455
carlastjuste <carlastjuste@users.noreply.github.com>
2021-03-17 02:24:45 -0400

    add weather.jsx

---
+++ src/components/weather.jsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+import React, { Component } from 'react';
+import API from "../utils/API";
+import Forecast from "./forecast";
+import CurrentWeather from "./currentWeather";
+
+
+class weather extends Component {
+    state = {
+        defaultCity: "Charlotte",
+        defaultCityLat:35.2271,
+        defaultCityLon:-80.8431,
+        defaultUnit:"imperial",
+        searchCity:"Charlotte",
+        unitTemp:"F",
+        unitWind:" MPH",
+        currentWeather: "",
+        forecast:""
+      };
+
+
+   async componentDidMount() {
+      const subUrl = `${this.state.defaultCityLat}&lon=${this.state.defaultCityLon}&units=${this.state.defaultUnit}`;
+      const {data: weatherData} = await API.oneCall(subUrl);
+      this.setState({currentWeather: weatherData.current,forecast: weatherData.daily})
+      console.log(weatherData.current);
+      console.log(weatherData.daily);
+  }
+  
+    
+    render() { 
+        return ( 
+            <React.Fragment>
+                <CurrentWeather currentWeatherData={this.state.currentWeather} searchCity= {this.state.searchCity} />
+                <Forecast forecastData={this.state.forecast}/>
+            </React.Fragment>
+         );
+    }
+}
+ 
+export default weather;
